b1dcb93b8ad4f09ebbafa0cde4d5b82efb2bfcc3
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-26 16:56:02 +0100

    shopping basket challenge completed

---
+++ ShoppingBasket.js
@@ -0,0 +1,16 @@
+require('./candy')
+class ShoppingBasket{
+  constructor(){
+    this.basket = [];
+  }
+  addItem(item) {
+    this.basket.push(item.getPrice());
+  }
+  getTotalPrice() {
+    let sum = 0
+    for(let i = 0; i < this.basket.length; i++){
+      sum += this.basket[i];
+    }
+    return sum;
+  }
+}
---
+++ candy.js
@@ -0,0 +1,12 @@
+class Candy{
+  constructor(name, price){
+    this.name = name;
+    this.price = price;
+  }
+  getName() {
+    return this.name;
+  }
+  getPrice(){
+    return this.price;
+  }
+}
