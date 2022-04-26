b1ca35582f9991fb7b6d248790a4d1e01f87d4b3
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-26 14:56:02 +0100

    user class example

---
+++ userClass.js
@@ -0,0 +1,11 @@
+class User {
+  constructor(name){
+    this.name = name;
+  }
+  getName(){
+    return this.name;
+  }
+  getIntroduction(){
+    return `Hi, my name is ${this.name}`
+  }
+}      
