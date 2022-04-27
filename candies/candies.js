31f3a43ef90c499a1f36cdfa88530c2644837606
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-27 09:30:17 +0100

    Refactored code

--- candies/candies.js
+++ candies/candies.js
@@ -1,45 +1,43 @@
 const candies = [
-  { name: 'Aero', price: 1.99 },
-  { name: 'Skitties', price: 2.99 },
-  { name: 'Mars', price: 1.49 },
-  { name: 'Maltesers', price: 3.49 },
-  { name: 'Skittles', price: 1.49 },
-  { name: 'Starburst', price: 5.99 },
-  { name: 'Ricola', price: 1.99 },
-  { name: 'Polkagris', price: 5.99 },
-  { name: 'Pastila', price: 4.99 },
-  { name: 'Mentos', price: 8.99 },
-  { name: 'Raffaello', price: 7.99 },
-  { name: 'Gummi bears', price: 10.99 },
-  { name: 'Fraise Tagada', price: 5.99 }
+  { name: "Aero", price: 1.99 },
+  { name: "Skitties", price: 2.99 },
+  { name: "Mars", price: 1.49 },
+  { name: "Maltesers", price: 3.49 },
+  { name: "Skittles", price: 1.49 },
+  { name: "Starburst", price: 5.99 },
+  { name: "Ricola", price: 1.99 },
+  { name: "Polkagris", price: 5.99 },
+  { name: "Pastila", price: 4.99 },
+  { name: "Mentos", price: 8.99 },
+  { name: "Raffaello", price: 7.99 },
+  { name: "Gummi bears", price: 10.99 },
+  { name: "Fraise Tagada", price: 5.99 },
 ];
 
-const searchCandies = (prefix, price) => { 
-  
+const searchCandies = (prefix, price) => {
   let prefix1 = 0;
   let prefix2 = 0;
   prefix1 = prefix.split("")[0];
   prefix2 = prefix.split("")[1];
-  if (prefix2 === undefined){
+  if (prefix2 === undefined) {
     return candies
-    .filter(candy => candy.price < price && (candy.name.split("")[0] === (prefix1.toUpperCase())))
-    .map(candy => `${candy.name}`);
+      .filter(
+        (candy) =>
+          candy.price < price &&
+          candy.name.split("")[0] === prefix1.toUpperCase()
+      )
+      .map((candy) => `${candy.name}`);
     console.log(candies);
   } else {
     return candies
-    .filter(candy => candy.price < price && (candy.name.split("")[0] === prefix1.toUpperCase() && candy.name.split("")[1] === prefix2))
-    .map(candy => `${candy.name}`);
+      .filter(
+        (candy) =>
+          candy.price < price &&
+          candy.name.split("")[0] === prefix1.toUpperCase() &&
+          candy.name.split("")[1] === prefix2
+      )
+      .map((candy) => `${candy.name}`);
   }
-}
+};
 
 module.exports = searchCandies;
-
-// class Candies{
-//   constructor(name,price){
-//     this.name = name;
-//     this.price = price;
-//   }
-//   searchCandies(prefix, price){
-
-//   }
-// }
--- candies/candies.test.js
+++ candies/candies.test.js
@@ -1,19 +1,23 @@
-const searchCandies = require('./candies');
+const searchCandies = require("./candies");
 
-describe('search for candies',() => {
-  it('retrun the candies with the asked prefix', () =>{
-    expect(searchCandies('Ma', 10)).toEqual(['Mars', 'Maltesers'])
+describe("search for candies", () => {
+  it("retrun the candies with the asked prefix", () => {
+    expect(searchCandies("Ma", 10)).toEqual(["Mars", "Maltesers"]);
   });
-  it('return Mars when price is under 2', () => {
-    expect(searchCandies('Ma',2)).toEqual(['Mars'])
+  it("return Mars when price is under 2", () => {
+    expect(searchCandies("Ma", 2)).toEqual(["Mars"]);
   });
-  it('return 3 products when S is selected and price is under 10', () => {
-    expect(searchCandies('S',10)).toEqual([ 'Skitties', 'Skittles', 'Starburst' ])
+  it("return 3 products when S is selected and price is under 10", () => {
+    expect(searchCandies("S", 10)).toEqual([
+      "Skitties",
+      "Skittles",
+      "Starburst",
+    ]);
   });
-  it('return 2 products when S is selected and price is under 4', () => {
-    expect(searchCandies('S',4)).toEqual([ 'Skitties', 'Skittles' ])
+  it("return 2 products when S is selected and price is under 4", () => {
+    expect(searchCandies("S", 4)).toEqual(["Skitties", "Skittles"]);
   });
-  it('return 2 products when ma is selected and price is under 10', () => {
-    expect(searchCandies('ma',10)).toEqual([ 'Mars', 'Maltesers' ])
+  it("return 2 products when ma is selected and price is under 10", () => {
+    expect(searchCandies("ma", 10)).toEqual(["Mars", "Maltesers"]);
   });
 });
