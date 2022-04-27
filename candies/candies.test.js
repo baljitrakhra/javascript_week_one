ef65634955c16c73a3619d9d3f3ccbdd0ab8065d
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-27 07:44:46 +0100

    candies challenge with all test passing. There is one feature pending - making is not case sensitive

---
+++ candies/candies.test.js
@@ -0,0 +1,16 @@
+const searchCandies = require('./candies');
+
+describe('search for candies',() => {
+  it('retrun the candies with the asked prefix', () =>{
+    expect(searchCandies('Ma', 10)).toEqual(['Mars', 'Maltesers'])
+  });
+  it('return Mars when price is under 2', () => {
+    expect(searchCandies('Ma',2)).toEqual(['Mars'])
+  });
+  it('return 3 products when S is selected and price is under 10', () => {
+    expect(searchCandies('S',10)).toEqual([ 'Skitties', 'Skittles', 'Starburst' ])
+  });
+  it('return 2 products when S is selected and price is under 4', () => {
+    expect(searchCandies('S',4)).toEqual([ 'Skitties', 'Skittles' ])
+  });
+});
