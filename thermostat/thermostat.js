054d48d2ac933740414b1d4978358b80408b86a5
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-27 19:32:37 +0100

    minimum temprature of 10 degree working now

--- thermostat/thermostat.js
+++ thermostat/thermostat.js
@@ -6,7 +6,11 @@ class Thermostat{
     return this.Temp += 1;  
   }
   down() {
+    if ( this.Temp <= 10 ){
+      return 'Thermostat is set on minimum temprature of 10 degree';
+    } else {
     return this.Temp -= 1;  
+  }
   }
   getTemprature(){
     return this.Temp;
--- thermostat/thermostat.test.js
+++ thermostat/thermostat.test.js
@@ -18,4 +18,11 @@ describe('Thermostate', () => {
     therm.getTemprature();
     expect(therm.Temp).toBe(21);
   });
+  it('returns a message that temp cannot go below 10 degree', () => {
+    const therm1 = new Thermostat();
+    for(i = 1; i < 11; i++) {
+      therm1.down();
+    }
+    expect(therm1.down()).toEqual('Thermostat is set on minimum temprature of 10 degree');
+  });
 });
