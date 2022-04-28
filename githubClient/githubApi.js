c771335f55a47759da1d38e5a650a5ed554e6bb8
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-28 14:50:54 +0100

    one more excercise from Phase 2

---
+++ githubClient/githubApi.js
@@ -0,0 +1,12 @@
+const got = require('got');
+
+class GithubApi{
+  fetchRepositoryData = (repo, callback) => {
+    got(`https://api.github.com/repos/${repo}`)
+    .then((response) =>{
+      callback(JSON.parse(response.body));
+    });
+  } 
+}
+
+module.exports = GithubApi;
