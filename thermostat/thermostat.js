69779762c12e4e08357077b76d637bc267f5d106
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-28 07:43:42 +0100

    changes implemented by preetier

--- thermostat/thermostat.js
+++ thermostat/thermostat.js
@@ -1,45 +1,44 @@
-class Thermostat{
-  constructor(){
+class Thermostat {
+  constructor() {
     this.Temp = 20;
     this.powerSMode = true;
   }
   up() {
-    if ( this.Temp >= 25 && this.powerSMode === true){
-      return 'Maximum temprature of 25 reached';
-    } else if ( this.Temp >= 32 && this.powerSMode === false) {
-      return 'Maximum temprature of 32 reached';
-    } else { 
-      return this.Temp += 1;
-  }
-      
+    if (this.Temp >= 25 && this.powerSMode === true) {
+      return "Maximum temprature of 25 reached";
+    } else if (this.Temp >= 32 && this.powerSMode === false) {
+      return "Maximum temprature of 32 reached";
+    } else {
+      return (this.Temp += 1);
+    }
   }
   down() {
-    if ( this.Temp <= 10 ){
-      return 'Thermostat is set on minimum temprature of 10 degree';
+    if (this.Temp <= 10) {
+      return "Thermostat is set on minimum temprature of 10 degree";
     } else {
-    return this.Temp -= 1;  
+      return (this.Temp -= 1);
+    }
   }
-  }
-  getTemperature(){
+  getTemperature() {
     return this.Temp;
   }
   setPowerSavingMode(condition) {
-    if (condition === false){
+    if (condition === false) {
       this.powerSMode = false;
-    }else {
-      this.powerSMode = true
+    } else {
+      this.powerSMode = true;
     }
   }
-  reset(){
-    this.Temp = 20
+  reset() {
+    this.Temp = 20;
   }
-  currentUsage(){
-    if (this.Temp < 18){
-      return 'Low usage';
+  currentUsage() {
+    if (this.Temp < 18) {
+      return "Low usage";
     } else if (this.Temp <= 25) {
-      return 'Medium usage';
+      return "Medium usage";
     } else {
-      return 'High usage';
+      return "High usage";
     }
   }
 }
--- thermostat/thermostat.test.js
+++ thermostat/thermostat.test.js
@@ -1,74 +1,76 @@
-const Thermostat = require('./thermostat');
+const Thermostat = require("./thermostat");
 
-describe('Thermostat', () => {
-  const therm = new Thermostat;
-  it('is initalised with an temperature of 20 degreess', () => {
+describe("Thermostat", () => {
+  const therm = new Thermostat();
+  it("is initalised with an temperature of 20 degreess", () => {
     expect(therm.Temp).toBe(20);
   });
-  it('lets you increase thermostat temperature by one degree', () => {
+  it("lets you increase thermostat temperature by one degree", () => {
     therm.up();
     expect(therm.Temp).toBe(21);
   });
-  it('lets you decrease thermostat temperature by one degree', () => {
+  it("lets you decrease thermostat temperature by one degree", () => {
     therm.up();
     therm.down();
     expect(therm.Temp).toBe(21);
   });
-  it('shows the current temprature', () => {
+  it("shows the current temprature", () => {
     therm.getTemperature();
     expect(therm.Temp).toBe(21);
   });
-  it('returns a message that temp cannot go below 10 degree', () => {
+  it("returns a message that temp cannot go below 10 degree", () => {
     const therm1 = new Thermostat();
-    for(i = 1; i < 11; i++) {
+    for (i = 1; i < 11; i++) {
       therm1.down();
     }
-    expect(therm1.down()).toEqual('Thermostat is set on minimum temprature of 10 degree');
+    expect(therm1.down()).toEqual(
+      "Thermostat is set on minimum temprature of 10 degree"
+    );
   });
-  it('set the power mode is true by default', () => {
+  it("set the power mode is true by default", () => {
     const therm2 = new Thermostat();
     expect(therm2.powerSMode).toEqual(true);
   });
-  it('powersaving mode can be turned off', () => {
+  it("powersaving mode can be turned off", () => {
     therm.setPowerSavingMode(false);
     expect(therm.powerSMode).toEqual(false);
   });
-  it('powersaving mode can be turned on', () => {
+  it("powersaving mode can be turned on", () => {
     therm.setPowerSavingMode(false);
     therm.setPowerSavingMode(true);
     expect(therm.powerSMode).toEqual(true);
   });
-  it('in power saving mode set the max temperature to 25', () => {
-    const therm3 = new Thermostat
-    for (let i = 0 ; i < 5 ; i++) {
+  it("in power saving mode set the max temperature to 25", () => {
+    const therm3 = new Thermostat();
+    for (let i = 0; i < 5; i++) {
       therm3.up();
     }
-    expect(therm3.up()).toEqual('Maximum temprature of 25 reached');
+    expect(therm3.up()).toEqual("Maximum temprature of 25 reached");
   });
-  it('in power saving mode off the max temperature set to 32', () => {
+  it("in power saving mode off the max temperature set to 32", () => {
     const therm4 = new Thermostat();
     therm4.setPowerSavingMode(false);
-    for (let i = 0 ; i < 12 ; i++) {
+    for (let i = 0; i < 12; i++) {
       therm4.up();
     }
-    expect(therm4.up()).toEqual('Maximum temprature of 32 reached');
+    expect(therm4.up()).toEqual("Maximum temprature of 32 reached");
   });
-  it('resets the temp t0 20', () => {
+  it("resets the temp t0 20", () => {
     therm.reset();
     expect(therm.Temp).toBe(20);
   });
-  it('shows low usuage', () => {
+  it("shows low usuage", () => {
     therm.reset();
     therm.down();
     therm.down();
     therm.down();
-    expect(therm.currentUsage()).toEqual('Low usage');
+    expect(therm.currentUsage()).toEqual("Low usage");
   });
-  it('shows medium usuage', () => {
+  it("shows medium usuage", () => {
     therm.reset();
-    expect(therm.currentUsage()).toEqual('Medium usage');
+    expect(therm.currentUsage()).toEqual("Medium usage");
   });
-  it('shows High usuage', () => {
+  it("shows High usuage", () => {
     therm.reset();
     therm.setPowerSavingMode(false);
     therm.up();
@@ -78,6 +80,6 @@ describe('Thermostat', () => {
     therm.up();
     therm.up();
     console.log(therm.Temp);
-    expect(therm.currentUsage()).toEqual('High usage');
+    expect(therm.currentUsage()).toEqual("High usage");
   });
 });
