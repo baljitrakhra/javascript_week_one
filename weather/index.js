0a3c32c2234b01135ca122abd9a81b9e393d73b0
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-28 17:45:52 +0100

    Weather api

--- weather/index.js
+++ weather/index.js
@@ -6,7 +6,8 @@ const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${
 let weatherData = null;
 got(apiUrl).then((response) => {
   weatherData = JSON.parse(response.body);
-  console.log(weatherData);
+  console.log(weatherData.main.temp);
+  console.log(weatherData.weather[0].main);
 });
 
 console.log('Requesting weather data');
