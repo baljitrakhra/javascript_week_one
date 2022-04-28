b6b05b1eeb8b0ee094bc463d2fd146f8662bceb8
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-28 17:38:44 +0100

    Weather api

---
+++ weather/index.js
@@ -0,0 +1,12 @@
+const got = require('got');
+const apiKey = '';
+const city = 'Jalandhar';
+const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
+
+let weatherData = null;
+got(apiUrl).then((response) => {
+  weatherData = JSON.parse(response.body);
+  console.log(weatherData);
+});
+
+console.log('Requesting weather data');
