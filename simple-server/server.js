a49f3855b86b6042e193cb1894c6f2fa268ebafd
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-29 16:46:32 +0100

    web app excercise completed

--- simple-server/server.js
+++ simple-server/server.js
@@ -5,11 +5,21 @@ const port = 3000;
 let counter = 0;
 
 app.get('/', (req, res) =>{
-  
-  res.send(`Counter: ${counter}`)
+  res.send(`Counter: ${counter}`);
 });
+
 app.post('/counter', (req, res) =>{
-    res.send(counter += 1);
+  counter += 1;
+  res.send('');
+  // res.redirect(307, '/increment');
+});
+
+app.get('/counter', (req, res) =>{
+  res.send(`Counter: ${counter}`);
+});
+app.delete('/counter', (req,res) => {
+  counter = 0
+  res.send('')
 });
 
 console.log(`Server Listning on localhost:${port}`);
