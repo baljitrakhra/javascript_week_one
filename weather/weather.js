f91ee7874e9c2dd4af1f72bebf5d892cb9b14d76
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-29 11:58:35 +0100

    weather bonus challenge

--- weather/weather.js
+++ weather/weather.js
@@ -3,19 +3,36 @@ require('./weatherapi')
 class Weather{
   constructor (api) {
     this.api = api
+    this.array = []
   }
 
 
 fetch = (city) => {
   this.api.fetchWeatherData(city, (weatherData) => {
     this.data = weatherData;
-    console.log(this.data)
   });
 }
 
 getWeatherData = () => {
   return this.data;
 }
+
+displayWeather = () =>{
+  console.log("City :", this.array[0]);
+  console.log("Weather :", this.array[1]);
+  console.log("Temprature :", this.array[2]);
+  console.log("Feels Like :", this.array[3]);
+  console.log("Humidity :", this.array[4]);
+}
+arrayFunction = () => {
+  this.array[0] = this.data.name;
+  this.array[1] = this.data.weather[0].description;
+  this.array[2] = this.data.main.temp;
+  this.array[3] = this.data.main.feels_like;
+  this.array[4] = this.data.main.humidity;
+
+}
+
 }
 
 module.exports = Weather;
--- weather/weather.test.js
+++ weather/weather.test.js
@@ -19,4 +19,29 @@ expect(weather.getWeatherData()).toEqual({
   description: 'some fake data'
 })
 });
+
+  it('displays weather in a better format', () => {
+  const mockedApi ={
+    fetchWeatherData: (cityname, callback) => {
+      callback({
+        City:         'London',
+        Weather:      'Clouds',
+        Temperature:  18.4,
+        Feels_like:   16.0,
+        Humidity:     '64%'
+      });
+    }
+  }
+
+  const weather = new Weather(mockedApi);
+  weather.fetch('Bristol');
+  weather.arrayFunction();
+  expect(weather.displayWeather()).toEqual( { City:         London,
+                                              Weather:      Clouds,
+                                              Temperature:  18.4,
+                                              Feels_like:   16.0,
+                                              Humidity:     '64%'
+                                            });
+
+  });
 });
